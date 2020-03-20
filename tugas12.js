611cf32fc9689ec611cec1614734d9492a91cf06
Mardionocs <mardionoputra19>
2020-03-21 02:22:23 +0700

    TUGAS 12 JS
    Tugas dari Niomic

---
+++ tugas12.js
@@ -0,0 +1,12 @@
+function tugasArray() {
+  var nama_buah = ['Pisang','Jeruk','Apel','Mangga'];
+  console.log(nama_buah);
+
+ nama_buah.pop();
+ console.log(nama_buah);
+
+ nama_buah.shift();
+ return nama_buah;
+
+}
+console.log(tugasArray());
